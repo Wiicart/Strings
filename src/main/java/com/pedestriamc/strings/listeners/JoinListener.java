4f20d69394bfe34675094fe8ec071480cd7a2faa
wiicart <wiicart@pedestriamc.com>
2024-07-10 22:37:50 -0500

    Update JoinListener.java

--- src/main/java/com/pedestriamc/strings/listeners/JoinListener.java
+++ src/main/java/com/pedestriamc/strings/listeners/JoinListener.java
@@ -2,6 +2,7 @@ package com.pedestriamc.strings.listeners;
 
 import com.pedestriamc.strings.User;
 import com.pedestriamc.strings.UserUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,8 +10,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinListener implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event){
-        User user = UserUtil.loadUser(event.getPlayer().getUniqueId());
-        if(user == null){
+        if(UserUtil.loadUser(event.getPlayer().getUniqueId()) == null){
+            Bukkit.getLogger().info("[Strings] Join Listener - loadUser returned null");
             new User(event.getPlayer().getUniqueId());
         }
     }
