9d49d3597d82a95a2a157a6e274bb634fdd5ace5
wiicart <wiicart@pedestriamc.com>
2024-07-11 16:44:57 -0500

    Update ChatListener.java

--- src/main/java/com/pedestriamc/strings/listeners/ChatListener.java
+++ src/main/java/com/pedestriamc/strings/listeners/ChatListener.java
@@ -2,6 +2,8 @@ package com.pedestriamc.strings.listeners;
 
 import com.pedestriamc.strings.ChatManager;
 import com.pedestriamc.strings.Strings;
+import com.pedestriamc.strings.message.Message;
+import com.pedestriamc.strings.message.Messenger;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +17,15 @@ public class ChatListener implements Listener {
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
         Player playerSender = event.getPlayer();
         String playerMessage = event.getMessage();
-        event.setFormat(chatManager.formatMessage(playerSender));
-        event.setMessage(chatManager.processMessage(playerSender,playerMessage));
+        if(chatManager.isOnCoolDown(playerSender)){
+            event.setCancelled(true);
+            Messenger.sendMessage(Message.COOL_DOWN, event.getPlayer());
+        }else{
+            event.setFormat(chatManager.formatMessage(playerSender));
+            event.setMessage(chatManager.processMessage(playerSender,playerMessage));
+            if(!event.isCancelled()){
+                chatManager.startCoolDown(playerSender);
+            }
+        }
     }
 }
