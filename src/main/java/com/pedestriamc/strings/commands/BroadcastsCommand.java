25f25605b96ea94b02677cf74b70e7850b08089d
wiicart <wiicart@pedestriamc.com>
2024-07-11 16:45:33 -0500

    Create BroadcastsCommand.java

---
+++ src/main/java/com/pedestriamc/strings/commands/BroadcastsCommand.java
@@ -0,0 +1,40 @@
+package com.pedestriamc.strings.commands;
+
+import com.pedestriamc.strings.Strings;
+import com.pedestriamc.strings.message.Message;
+import com.pedestriamc.strings.message.Messenger;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class BroadcastCommand implements CommandExecutor {
+
+    private final String broadcastFormat = Strings.getInstance().getBroadcastFormat();
+    private final boolean usePAPI = Strings.getInstance().usePlaceholderAPI();
+
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args){
+        if(!sender.hasPermission("strings.chat.broadcast") && !sender.hasPermission("strings.chat.*") && !sender.hasPermission("strings.*")){
+            Messenger.sendMessage(Message.NO_PERMS, sender);
+            return true;
+        }
+        StringBuilder broadcast = new StringBuilder(broadcastFormat);
+        String broadCastString;
+        if(sender.hasPermission("strings.chat.placeholdermsg") && usePAPI && !(sender instanceof Server)){
+            PlaceholderAPI.setPlaceholders((Player) sender, broadcast.toString());
+        }
+        for(String arg : args){
+            broadcast.append(arg);
+            broadcast.append(" ");
+        }
+        broadCastString = broadcast.toString();
+        broadCastString = ChatColor.translateAlternateColorCodes('&', broadCastString);
+        Bukkit.broadcastMessage(broadCastString);
+        return true;
+    }
+}
