cbd6706ab7f05672daf63eebd196fe7e831235a0
wiicart <wiicart@pedestriamc.com>
2024-07-12 13:06:16 -0500

    Update SocialSpyCommand.java

--- src/main/java/com/pedestriamc/strings/commands/SocialSpyCommand.java
+++ src/main/java/com/pedestriamc/strings/commands/SocialSpyCommand.java
@@ -4,6 +4,7 @@ import com.pedestriamc.strings.SocialSpy;
 import com.pedestriamc.strings.Strings;
 import com.pedestriamc.strings.message.Message;
 import com.pedestriamc.strings.message.Messenger;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,35 +21,50 @@ public class SocialSpyCommand implements CommandExecutor {
             Messenger.sendMessage(Message.NO_PERMS, sender);
             return true;
         }
+        if(args.length == 0){
+            noArgs(sender);
+            return true;
+        }
+        if(sender instanceof Server){
+            sender.sendMessage("[Strings] This command can only be used by players");
+            return true;
+        }
         if(args.length > 1){
             Messenger.sendMessage(Message.TOO_MANY_ARGS, sender);
             return true;
         }
         switch(args[0]){
             case "on", "true" -> {
-                strings.getUser((Player) sender).setSocialSpy(true);
-                socialSpy.addSpy(sender);
-                Messenger.sendMessage(Message.SOCIAL_SPY_ON, sender);
+                enableSocialSpy(sender);
                 return true;
             }
             case "off", "false" -> {
-                strings.getUser((Player) sender).setSocialSpy(false);
-                socialSpy.removeSpy(sender);
-                Messenger.sendMessage(Message.SOCIAL_SPY_OFF, sender);
+                disableSocialSpy(sender);
                 return true;
             }
             default -> {
-                if(strings.getUser((Player) sender).isSocialSpy()){
-                    strings.getUser((Player) sender).setSocialSpy(true);
-                    socialSpy.removeSpy(sender);
-                    Messenger.sendMessage(Message.SOCIAL_SPY_OFF, sender);
-                    return true;
-                }
-                strings.getUser((Player) sender).setSocialSpy(false);
-                socialSpy.removeSpy(sender);
-                Messenger.sendMessage(Message.SOCIAL_SPY_OFF, sender);
+                noArgs(sender);
                 return true;
             }
         }
+    }
+
+    public void enableSocialSpy(CommandSender sender){
+        strings.getUser((Player) sender).setSocialSpy(true);
+        socialSpy.addSpy(sender);
+        Messenger.sendMessage(Message.SOCIAL_SPY_ON, sender);
+    }
+
+    public void disableSocialSpy(CommandSender sender){
+        strings.getUser((Player) sender).setSocialSpy(false);
+        socialSpy.removeSpy(sender);
+        Messenger.sendMessage(Message.SOCIAL_SPY_OFF, sender);
+    }
+    public void noArgs(CommandSender sender){
+        if(strings.getUser((Player) sender).isSocialSpy()){
+            disableSocialSpy(sender);
+            return;
+        }
+        enableSocialSpy(sender);
     }
 }
