eeb1a7f3a4da7a12d8fd2006f7f7e7a71d11c4ed
wiicart <wiicart@pedestriamc.com>
2024-07-12 15:53:45 -0500

    Update Channel.java

--- src/main/java/com/pedestriamc/strings/channels/Channel.java
+++ src/main/java/com/pedestriamc/strings/channels/Channel.java
@@ -6,26 +6,44 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Channel {
 
     private String name;
+    private String format;
+    private String defaultColor;
     private final Set<Player> members;
     private final ChatManager chatManager;
+    private boolean active;
 
-    public Channel(Strings strings, String name, Set<Player> members){
+    public Channel(Strings strings, String name, String format, String defaultColor){
         this.name = name;
-        this.members = members;
+        this.members = new HashSet<>();
+        this.format = format;
+        this.defaultColor = defaultColor;
         this.chatManager = strings.getChatManager();
+        this.active = true;
+        strings.getChannelManager().registerChannel(this);
     }
 
     public void sendMessage(Player player, String message){
-        String format = chatManager.formatMessage(player);
-        message = chatManager.processMessage(player, message);
-        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(false, player, message, members);
-        event.setFormat(format);
-        Bukkit.getPluginManager().callEvent(event);
+        if(active){
+            String format = chatManager.formatMessage(player);
+            message = chatManager.processMessage(player, message);
+            AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(false, player, message, members);
+            event.setFormat(format);
+            Bukkit.getPluginManager().callEvent(event);
+        }
+    }
+
+    public String getFormat(){
+        return format;
+    }
+
+    public String getDefaultColor(){
+        return defaultColor;
     }
 
     public String getName(){
@@ -42,5 +60,9 @@ public class Channel {
 
     public void removePlayer(Player player){
         members.remove(player);
+    }
+
+    public void closeChannel(){
+        Strings.getInstance().getChannelManager().unregisterChannel(this);
     }
 }
