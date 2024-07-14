b1ab9a70beee39397643c85c00858550637c6d9a
wiicart <wiicart@pedestriamc.com>
2024-07-14 15:46:42 -0500

    Update ChannelCommand.java

--- src/main/java/com/pedestriamc/strings/commands/ChannelCommand.java
+++ src/main/java/com/pedestriamc/strings/commands/ChannelCommand.java
@@ -15,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class ChannelCommand implements CommandExecutor {
 
-    private Strings strings = Strings.getInstance();
+    private final Strings strings = Strings.getInstance();
 
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args){
         if(args.length > 3){
@@ -31,7 +31,10 @@ public class ChannelCommand implements CommandExecutor {
                     //help message
                     return true;
                 }
-                setActiveChannel(sender, args[0]);
+                if(sender instanceof Server){
+                    sender.sendMessage("[Strings] This command cannot be used with 1 arg as server.");
+                }
+                setActiveChannel(sender, args[0], (Player) sender);
                 return true;
             }
             case 2 -> {
@@ -39,7 +42,7 @@ public class ChannelCommand implements CommandExecutor {
                 the command has two args, proper usage will be trying to join or leave a channel, or setting the active
                 channel of another player.
                  */
-                switch(args[1].toUpperCase()){
+                switch(args[0].toUpperCase()){
                     case "JOIN" -> {
                         if(sender instanceof Server){
                             sender.sendMessage("[Strings] /channel join must be used with a player parameter");
@@ -59,23 +62,21 @@ public class ChannelCommand implements CommandExecutor {
                         return true;
                     }
                     default -> {
-                        if(Bukkit.getPlayer(args[1]) == null){
-                            Player p = Bukkit.getPlayer(args[1]);
+                        Player p = Bukkit.getPlayer(args[1]);
+                        if(p == null){
+                            Messenger.channelCmdMessage(Message.INVALID_PLAYER, sender, args[1], null);
+                            return true;
+                        }else{
+                            setActiveChannel(sender, args[0], p);
                             return true;
                         }
-                        if(strings.getCommand(args[0]) != null){
-                            Messenger.sendMessage(Message.INVALID_PLAYER, sender);
-                            return true;
-                        }
-                        Messenger.sendMessage(Message.INVALID_USE_CHANNEL, sender);
-                        return true;
                     }
                 }
             }
             case 3 -> {
                 if(args[1].equalsIgnoreCase("join")){
                     if(Bukkit.getPlayer(args[2]) == null){
-                        Messenger.sendMessage(Message.INVALID_PLAYER, sender);
+                        Messenger.channelCmdMessage(Message.INVALID_PLAYER, sender, args[2], null);
                         return true;
                     }
                     joinChannel(sender, args[1], Bukkit.getPlayer(args[2]));
@@ -83,7 +84,7 @@ public class ChannelCommand implements CommandExecutor {
                 }
                 if(args[1].equalsIgnoreCase("leave")){
                     if(Bukkit.getPlayer(args[2]) == null){
-                        Messenger.sendMessage(Message.INVALID_PLAYER, sender);
+                        Messenger.channelCmdMessage(Message.INVALID_PLAYER, sender, args[2], null);
                         return true;
                     }
                     leaveChannel(sender, args[1], Bukkit.getPlayer(args[2]));
@@ -98,70 +99,82 @@ public class ChannelCommand implements CommandExecutor {
             }
         }
     }
-    private void setActiveChannel(CommandSender sender, String channelName){
-        if(sender instanceof Server){
-            sender.sendMessage("[Strings] This command cannot be used with 1 arg as server.");
+    private void setActiveChannel(CommandSender sender, String channelName, Player target){
+        boolean modifyingOther = !sender.equals(target);
+        if(modifyingOther && !sender.hasPermission("strings.channel.modifyplayers")){
+            Messenger.sendMessage(Message.NO_PERMS, sender);
             return;
         }
-        if(sender.hasPermission("strings.channel." + channelName) || sender.hasPermission("strings.channel.*") || sender.hasPermission("strings.*")){
-            Channel channel = strings.getChannel(channelName);
-            if(channel != null){
-                strings.getUser((Player) sender).setActiveChannel(channel);
-                Messenger.sendMessage(Message.CHANNEL_ACTIVE, sender);
+        Channel c = strings.getChannel(channelName);
+        if(c == null){
+            Messenger.channelCmdMessage(Message.CHANNEL_DOES_NOT_EXIST, sender, target.getName(), channelName);
+            return;
+        }
+        if(modifyingOther && !target.hasPermission("strings.channels." + c.getName())){
+            Messenger.channelCmdMessage(Message.OTHER_PLAYER_NO_PERMS, sender, target.getName(), channelName);
+            return;
+        }
+        if((!target.hasPermission("strings.channels.channel" + channelName) && !target.hasPermission("strings.channels.*") && !target.hasPermission("strings.*") && !target.hasPermission("strings.channels.channel.*"))){
+            if(modifyingOther){
+                Messenger.channelCmdMessage(Message.OTHER_PLAYER_NO_PERMS, sender, target.getName(), channelName);
+            }else{
+                Messenger.channelCmdMessage(Message.NO_PERMS_CHANNEL, sender, sender.getName(), channelName);
             }
             return;
         }
-        Messenger.sendMessage(Message.NO_PERMS, sender);
+        User user = strings.getUser(target);
+        user.setActiveChannel(c);
+        if(modifyingOther){
+            Messenger.channelCmdMessage(Message.OTHER_PLAYER_CHANNEL_ACTIVE, sender, target.getName(), channelName);
+        }
+        Messenger.channelCmdMessage(Message.CHANNEL_ACTIVE, target, target.getName(), channelName);
+
     }
 
     private void joinChannel(@NotNull CommandSender sender, String channel, Player target){
         boolean modifyingOther = !sender.equals(target);
         //Check if sender has permission to modify other players:
-        if(modifyingOther && sender.hasPermission("strings.channels.modifyplayers")){
+        if(modifyingOther && !sender.hasPermission("strings.channel.modifyplayers")){
             Messenger.sendMessage(Message.NO_PERMS, sender);
             return;
         }
         // Check if channel exists:
         Channel c = strings.getChannel(channel);
         if(c == null){
-            Messenger.sendMessage(Message.CHANNEL_DOES_NOT_EXIST, sender);
+            Messenger.channelCmdMessage(Message.CHANNEL_DOES_NOT_EXIST, sender, target.getName(), channel);
             return;
         }
         if(modifyingOther && !target.hasPermission("strings.channels." + c.getName())){
-            Messenger.sendMessage(Message.OTHER_PLAYER_NO_PERMS, sender);
+            Messenger.channelCmdMessage(Message.OTHER_PLAYER_NO_PERMS, sender, target.getName(), channel);
             return;
         }
         User user = strings.getUser(target);
         user.joinChannel(c);
         if(modifyingOther){
-            Messenger.sendMessage(Message.OTHER_USER_JOINED_CHANNEL, sender);
-            Messenger.sendMessage(Message.CHANNEL_JOINED, target);
-            return;
+            Messenger.channelCmdMessage(Message.OTHER_USER_JOINED_CHANNEL, sender, target.getName(), channel);
         }
-        Messenger.sendMessage(Message.CHANNEL_JOINED, target);
+        Messenger.channelCmdMessage(Message.CHANNEL_JOINED, target, target.getName(), channel);
     }
 
     private void leaveChannel(@NotNull CommandSender sender, String channel, Player target){
         boolean modifyingOther = !sender.equals(target);
         //Check if sender has permission to modify other players:
-        if(modifyingOther && sender.hasPermission("strings.channels.modifyplayers")){
+        if(modifyingOther && !sender.hasPermission("strings.channels.modifyplayers")){
             Messenger.sendMessage(Message.NO_PERMS, sender);
             return;
         }
         // Check if channel exists:
         Channel c = strings.getChannel(channel);
         if(c == null){
-            Messenger.sendMessage(Message.CHANNEL_DOES_NOT_EXIST, sender);
+            Messenger.channelCmdMessage(Message.CHANNEL_DOES_NOT_EXIST, sender, target.getName(), channel);
             return;
         }
         User user = strings.getUser(target);
         user.leaveChannel(c);
         if(modifyingOther){
-            Messenger.sendMessage(Message.OTHER_USER_LEFT_CHANNEL, sender);
-            Messenger.sendMessage(Message.LEFT_CHANNEL, target);
-            return;
+            Messenger.channelCmdMessage(Message.OTHER_USER_LEFT_CHANNEL, sender, target.getName(), channel);
         }
-        Messenger.sendMessage(Message.LEFT_CHANNEL, target);
+        Messenger.channelCmdMessage(Message.LEFT_CHANNEL, target, target.getName(), channel);
     }
 }
 
