af7680700f90a06616f31a59f491fb46881d0a8d
wiicart <wiicart@pedestriamc.com>
2024-07-15 12:59:33 -0500

    Update ChatListener.java

--- src/main/java/com/pedestriamc/strings/listeners/ChatListener.java
+++ src/main/java/com/pedestriamc/strings/listeners/ChatListener.java
@@ -3,15 +3,14 @@ package com.pedestriamc.strings.listeners;
 import com.pedestriamc.strings.ChatManager;
 import com.pedestriamc.strings.Strings;
 import com.pedestriamc.strings.User;
+import com.pedestriamc.strings.channels.Channel;
 import com.pedestriamc.strings.message.Message;
 import com.pedestriamc.strings.message.Messenger;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public class ChatListener implements Listener {
 
@@ -19,29 +18,34 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
-        Bukkit.getScheduler().runTask(Strings.getInstance(), () -> { //https://www.spigotmc.org/threads/asyncplayerchatevent-may-only-be-triggered-synchronously.611643/
-            Player playerSender = event.getPlayer();
-            String playerMessage = event.getMessage();
+        Player playerSender = event.getPlayer();
+        String playerMessage = event.getMessage();
 
-            //this signifies that this event came from a Channel, and has been processed
-            if(event.getFormat().contains("" + ChatColor.AQUA + ChatColor.AQUA + ChatColor.RESET)){
-                Bukkit.getLogger().info("detected msg already processed");
-                return;
-            }
-            User user = Strings.getInstance().getUser(playerSender);
-            if(chatManager.isOnCoolDown(playerSender)){
-                event.setCancelled(true);
-                Messenger.sendMessage(Message.COOL_DOWN, event.getPlayer());
-            }else{
-                //cancel event, as it is being replaced by this plugin
-                Bukkit.getLogger().info("Attempting to replace msg event");
+        //this signifies that this event came from a Channel, and has been processed
+        if(event.getFormat().contains("" + ChatColor.AQUA + ChatColor.AQUA + ChatColor.RESET)){
+            return;
+        }
+        User user = Strings.getInstance().getUser(playerSender);
+        if(chatManager.isOnCoolDown(playerSender)){
+            cancelEvent(event);
+            Messenger.sendMessage(Message.COOL_DOWN, event.getPlayer());
+        }else{
+            //cancel event, as it is being replaced by this plugin
+            Channel c = user.getActiveChannel();
+            if(c != null){
                 user.getActiveChannel().sendMessage(playerSender,playerMessage);
-                event.setCancelled(true);
-                if(!event.isCancelled()){
-                    chatManager.startCoolDown(playerSender);
-                }
+            }else{
+                Strings.getInstance().getChannel("global").sendMessage(playerSender,playerMessage);
+                user.leaveChannel(user.getActiveChannel());
             }
+            cancelEvent(event);
+            if(!event.isCancelled()){
+                chatManager.startCoolDown(playerSender);
+            }
+        }
+    }
 
-        });
+    public void cancelEvent(AsyncPlayerChatEvent event){
+        event.setCancelled(true);
     }
 }
