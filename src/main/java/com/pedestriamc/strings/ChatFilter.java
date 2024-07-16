47c12353a02790cc72ab0299493e2ec39ade9021
wiicart <wiicart@pedestriamc.com>
2024-07-16 12:48:06 -0500

    Update ChatFilter.java

--- src/main/java/com/pedestriamc/strings/ChatFilter.java
+++ src/main/java/com/pedestriamc/strings/ChatFilter.java
@@ -4,6 +4,7 @@ import com.pedestriamc.strings.message.Message;
 import com.pedestriamc.strings.message.Messenger;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
@@ -15,13 +16,22 @@ public class ChatFilter{
     private final boolean doProfanityFilter;
     private final String regex = "(?i)\\b((?:https?|ftp)://|www\\.)?[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(\\S*)?\\b";
     private final Pattern pattern = Pattern.compile(regex);
-    private List<String> urlWhitelist;
+    private final List<String> urlWhitelist;
     private List<String> bannedWords;
 
     public ChatFilter(Strings strings){
         this.strings = strings;
-        this.doLinkFilter = strings.isBlockUrls();
-        this.doProfanityFilter = strings.isBlockProfanity();
+        this.doLinkFilter = strings.getConfig().getBoolean("block-urls", false);
+        this.doProfanityFilter = strings.getConfig().getBoolean("filter-profanity", false);
+        this.urlWhitelist = new ArrayList<>();
+        List<?> tempList = strings.getConfig().getList("url-whitelist");
+        if(tempList != null)
+        for(Object obj : tempList){
+            if(obj instanceof String) {
+                this.urlWhitelist.add((String) obj);
+
+            }
+        }
     }
 
     public String filter(String msg, Player player){
@@ -39,12 +49,6 @@ public class ChatFilter{
         if(urlReplaced){
             Messenger.sendMessage(Message.LINKS_PROHIBITED, player);
         }
-        if(doProfanityFilter){
-
-        }
-
-
-
         return msg;
     }
 
