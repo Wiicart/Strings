7c19c56137c586a5d7342de8e1c8084b43e51d96
wiicart <wiicart@pedestriamc.com>
2024-07-16 10:56:14 -0500

    Create ChatFilter.java

---
+++ src/main/java/com/pedestriamc/strings/ChatFilter.java
@@ -0,0 +1,51 @@
+package com.pedestriamc.strings;
+
+import com.pedestriamc.strings.message.Message;
+import com.pedestriamc.strings.message.Messenger;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ChatFilter{
+    private final Strings strings;
+    private final boolean doLinkFilter;
+    private final boolean doProfanityFilter;
+    private final String regex = "(?i)\\b((?:https?|ftp)://|www\\.)?[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(\\S*)?\\b";
+    private final Pattern pattern = Pattern.compile(regex);
+    private List<String> urlWhitelist;
+    private List<String> bannedWords;
+
+    public ChatFilter(Strings strings){
+        this.strings = strings;
+        this.doLinkFilter = strings.isBlockUrls();
+        this.doProfanityFilter = strings.isBlockProfanity();
+    }
+
+    public String filter(String msg, Player player){
+        boolean urlReplaced = false;
+        if(doLinkFilter){
+            Matcher matcher = pattern.matcher(msg);
+            List<MatchResult> results = matcher.results().toList();
+            for(MatchResult result : results){
+                if(!urlWhitelist.contains(result.toString())){
+                    msg = msg.replace(result.toString(), "");
+                    urlReplaced = true;
+                }
+            }
+        }
+        if(urlReplaced){
+            Messenger.sendMessage(Message.LINKS_PROHIBITED, player);
+        }
+        if(doProfanityFilter){
+
+        }
+
+
+
+        return msg;
+    }
+
+}
