147f62249879eceb27a8010deb13b8b5496514e0
wiicart <wiicart@pedestriamc.com>
2024-07-17 18:42:38 -0500

    Update ServerMessages.java

--- src/main/java/com/pedestriamc/strings/ServerMessages.java
+++ src/main/java/com/pedestriamc/strings/ServerMessages.java
@@ -1,14 +1,20 @@
-package com.pedestriamc.strings;
+package com.pedestriamc.stringscustoms;
 
+import com.pedestriamc.strings.Strings;
+import com.pedestriamc.strings.User;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerMessages {
 
     private final Strings strings;
     private final String joinMessageTemplate;
     private final String leaveMessageTemplate;
+    private final ArrayList<String> motd;
     private final boolean usePAPI;
 
     public ServerMessages(Strings strings){
@@ -16,6 +22,15 @@ public class ServerMessages {
         this.joinMessageTemplate = strings.getConfig().getString("join-message");
         this.leaveMessageTemplate = strings.getConfig().getString("leave-message");
         this.usePAPI = strings.usePlaceholderAPI();
+        this.motd = new ArrayList<>();
+        List<?> list = strings.getConfig().getList("motd");
+        if(list != null){
+            for(Object obj : list){
+                if(obj instanceof String){
+                    motd.add((String) obj);
+                }
+            }
+        }
     }
     public String joinMessage(Player player){
         String message = joinMessageTemplate;
@@ -43,5 +58,23 @@ public class ServerMessages {
     }
     public String deathMessage(Player player){
         return null;
+    }
+
+    public void sendMOTD(Player player){
+        ArrayList<String> playerMOTD = new ArrayList<>(motd);
+        User user = strings.getUser(player);
+        for(String message: playerMOTD){
+            message = message.replace("{displayname}", player.getName());
+            message = message.replace("{username}", player.getName());
+            message = message.replace("{prefix}", user.getPrefix());
+            message = message.replace("{suffix}", user.getSuffix());
+            if(usePAPI){
+                message = PlaceholderAPI.setPlaceholders(player, message);
+            }
+            message = ChatColor.translateAlternateColorCodes('&', message);
+            player.sendMessage(message);
+        }
+
+
     }
 }
