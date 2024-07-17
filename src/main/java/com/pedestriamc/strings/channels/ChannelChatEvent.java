d9bfcd42fee877849a8e7043f3a3d101449c86c1
wiicart <wiicart@pedestriamc.com>
2024-07-17 17:37:46 -0500

    Create ChannelChatEvent.java

---
+++ src/main/java/com/pedestriamc/strings/channels/ChannelChatEvent.java
@@ -0,0 +1,27 @@
+package com.pedestriamc.strings.channels;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class ChannelChatEvent extends AsyncPlayerChatEvent {
+
+    private final Channel channel;
+
+    /**
+     * @param async   This changes the event to a synchronous state.
+     * @param who     the chat sender
+     * @param message the message sent
+     * @param players the players to receive the message. This may be a lazy
+     */
+    public ChannelChatEvent(boolean async, @NotNull Player who, @NotNull String message, @NotNull Set<Player> players, Channel channel) {
+        super(async, who, message, players);
+        this.channel = channel;
+    }
+
+    public Channel getChannel(){
+        return channel;
+    }
+}
