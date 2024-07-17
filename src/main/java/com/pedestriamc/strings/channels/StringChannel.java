1bfc9f83e936cdea92b21bc61d003f8a505828ac
wiicart <wiicart@pedestriamc.com>
2024-07-17 18:40:40 -0500

    Create StringChannel.java

---
+++ src/main/java/com/pedestriamc/strings/channels/StringChannel.java
@@ -0,0 +1,129 @@
+package com.pedestriamc.strings.channels;
+
+import com.pedestriamc.strings.ChatManager;
+import com.pedestriamc.strings.Strings;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class StringChannel implements Channel{
+
+    private final Strings strings;
+    private String name;
+    private String format;
+    private String defaultColor;
+    private final boolean callEvent;
+    private final Set<Player> members;
+    private final ChatManager chatManager;
+    private volatile boolean active;
+
+    public StringChannel(@NotNull Strings strings, String name, String format, String defaultColor, ChannelManager channelManager, boolean callEvent){
+        this.strings = strings;
+        this.name = name;
+        this.members = ConcurrentHashMap.newKeySet();
+        this.format = format;
+        this.defaultColor = defaultColor != null ? defaultColor : "&f";
+        this.chatManager = strings.getChatManager();
+        this.active = true;
+        this.callEvent = callEvent;
+        channelManager.registerChannel(this);
+    }
+
+    //Send player messages in the channel.
+    //The message will be sent to all players that are a member of the channel,
+    //which includes players that don't have this channel as active.
+    //Also calls a AsyncPlayerChatEvent so that other plugins can handle this message
+    @Override
+    public void sendMessage(Player player, String message){
+        if(active){
+            String format = chatManager.formatMessage(player, this);
+            message = chatManager.processMessage(player, message);
+            String finalMessage = message;
+            if(callEvent){
+                Bukkit.getScheduler().runTask(strings, () ->{
+                    AsyncPlayerChatEvent event = new ChannelChatEvent(false, player, finalMessage, members, this);
+                    event.setFormat(format);
+                    Bukkit.getPluginManager().callEvent(event);
+                    if(!event.isCancelled()){
+                        String formattedMessage = String.format(event.getFormat(), strings.getUser(player).getDisplayName(), event.getMessage());
+                        for(Player p : members){
+                            p.sendMessage(formattedMessage);
+                        }
+                        Bukkit.getLogger().info(ChatColor.stripColor(formattedMessage));
+                    }
+                });
+            }else{
+                String formattedMessage = String.format(format, player.getDisplayName(), message);
+                for(Player p : members){
+                    p.sendMessage(formattedMessage);
+                }
+                Bukkit.getLogger().info(ChatColor.stripColor(formattedMessage));
+            }
+        }
+    }
+
+    //Broadcasts a message to members of this channel. (Auto Broadcasts)
+    @Override
+    public void broadcastMessage(String message){
+        for(Player p : members){
+            p.sendMessage(message);
+        }
+    }
+
+    // Closes this channel, updates players to reflect this channel closing, and unregisters channel
+    // TODO: remove this channel from the config when this method is called
+    @Override
+    public void closeChannel(){
+        Strings.getInstance().getChannelManager().unregisterChannel(this);
+        active = false;
+    }
+
+    @Override
+    public String getFormat(){
+        return format;
+    }
+
+    @Override
+    public String getDefaultColor(){
+        return defaultColor;
+    }
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void setDefaultColor(String defaultColor){
+        this.defaultColor = defaultColor;
+    }
+
+    @Override
+    public void setFormat(String format){ this.format = format; }
+
+    @Override
+    public void addPlayer(Player player){
+        members.add(player);
+    }
+
+    @Override
+    public void removePlayer(Player player){
+        members.remove(player);
+    }
+
+    @Override
+    public Set<Player> getMembers(){
+        return members;
+    }
+
+}
