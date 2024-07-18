0b8606a70cc90e4d3ded287f6c8025460063c2e1
wiicart <wiicart@pedestriamc.com>
2024-07-17 22:38:38 -0500

    Create MessageTabCompleter.java

---
+++ src/main/java/com/pedestriamc/strings/tabcompleters/MessageTabCompleter.java
@@ -0,0 +1,27 @@
+package com.pedestriamc.strings.tabcompleters;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageTabCompleter implements TabCompleter {
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if(args.length <= 1){
+            ArrayList<String> list = new ArrayList<>();
+            for(Player p : Bukkit.getOnlinePlayers()){
+                list.add(p.getName());
+            }
+            return list;
+        }
+        return new ArrayList<>();
+    }
+}
