b8748e72a85ebe4f693e7fffae39f58a393f7ddd
wiicart <wiicart@pedestriamc.com>
2024-07-18 16:19:55 -0500

    Update ChannelTabCompleter.java

--- src/main/java/com/pedestriamc/strings/tabcompleters/ChannelTabCompleter.java
+++ src/main/java/com/pedestriamc/strings/tabcompleters/ChannelTabCompleter.java
@@ -21,17 +21,14 @@ public class ChannelTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String @NotNull [] args) {
         if(args.length <= 1){
-            List<String> list = channelManager.getChannelNames();
+            List<String> list = channelManager.getNonProtectedChannelNames();
             list.add("join");
             list.add("leave");
-            list.remove("helpop");
             return list;
         }
         if(args.length == 2){
             if(args[0].equalsIgnoreCase("join") || args[0].equalsIgnoreCase("leave")){
-                List<String> list = channelManager.getChannelNames();
-                list.remove("helpop");
-                return list;
+                return channelManager.getNonProtectedChannelNames();
             }
             ArrayList<String> list = new ArrayList<>();
             for(Player p : Bukkit.getOnlinePlayers()){
