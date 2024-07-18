6f63d6520c08e31d8017d12753edfb79e037c198
wiicart <wiicart@pedestriamc.com>
2024-07-17 22:38:08 -0500

    Create ClearChatTabCompleter.java

---
+++ src/main/java/com/pedestriamc/strings/tabcompleters/ClearChatTabCompleter.java
@@ -0,0 +1,21 @@
+package com.pedestriamc.strings.tabcompleters;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClearChatTabCompleter implements TabCompleter {
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if(args.length <= 1){
+            return List.of("all");
+        }
+        return new ArrayList<>();
+    }
+}
