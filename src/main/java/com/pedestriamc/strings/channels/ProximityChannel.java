fa7e7e62e65bf277c72e22f4ddd929633e40516e
wiicart <wiicart@pedestriamc.com>
2024-07-23 11:06:17 -0500

    Update ProximityChannel.java

--- src/main/java/com/pedestriamc/strings/channels/ProximityChannel.java
+++ src/main/java/com/pedestriamc/strings/channels/ProximityChannel.java
@@ -1,93 +1,165 @@
 package com.pedestriamc.strings.channels;
 
+import com.pedestriamc.strings.ChatManager;
 import com.pedestriamc.strings.Strings;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProximityChannel implements Channel{
 
-    public ProximityChannel(@NotNull Strings strings, String name, String format, String defaultColor, ChannelManager channelManager, boolean callEvent){
+    private final Strings strings;
+    private final Set<Player> members;
+    private String name;
+    private String format;
+    private String defaultColor;
+    private final ChannelManager channelManager;
+    private final ChatManager chatManager;
+    private boolean callEvent;
+    private boolean doURLFilter;
+    private boolean doProfanityFilter;
+    private boolean doCooldown;
+    private int distance;
+
+    public ProximityChannel(@NotNull Strings strings, String name, String format, String defaultColor, ChannelManager channelManager, ChatManager chatManager, boolean callEvent, boolean doURLFilter, boolean doProfanityFilter, boolean doCooldown, int distance){
+        this.strings = strings;
+        this.name = name;
+        this.format = format;
+        this.defaultColor = defaultColor;
+        this.channelManager = channelManager;
+        this.chatManager = chatManager;
+        this.callEvent = callEvent;
+        this.doURLFilter = doURLFilter;
+        this.doProfanityFilter = doProfanityFilter;
+        this.doCooldown = doCooldown;
+        this.distance = distance;
+        this.members = ConcurrentHashMap.newKeySet();
         channelManager.registerChannel(this);
     }
 
     @Override
-    public void sendMessage(Player player, String message) {
-        ArrayList<Player> receivers = new ArrayList<>();
+    public void sendMessage(@NotNull Player player, String message) {
 
+        HashSet<Player> receivers = new HashSet<>(getRecipients(player));
+        receivers.addAll(members);
+        String format = chatManager.formatMessage(player, this);
+        message = chatManager.processMessage(player, message);
+        String finalMessage = message;
+
+        if(callEvent){
+            Bukkit.getScheduler().runTask(strings, () ->{
+                AsyncPlayerChatEvent event = new ChannelChatEvent(false, player, finalMessage, receivers, this);
+                event.setFormat(format);
+                Bukkit.getPluginManager().callEvent(event);
+                if(!event.isCancelled()){
+                    String formattedMessage = String.format(event.getFormat(), strings.getUser(player).getDisplayName(), event.getMessage());
+                    for(Player p : receivers){
+                        p.sendMessage(formattedMessage);
+                    }
+                    Bukkit.getLogger().info(ChatColor.stripColor(formattedMessage));
+                }
+            });
+            return;
+        }
+
+        String formattedMessage = String.format(format, player.getDisplayName(), message);
+        for(Player p : receivers){
+            p.sendMessage(formattedMessage);
+        }
+        Bukkit.getLogger().info(ChatColor.stripColor(formattedMessage));
+
+
+    }
+
+    //https://www.spigotmc.org/threads/getting-player-distance-to-another-player.343523/
+    private @NotNull HashSet<Player> getRecipients(Player sender){
+        HashSet<Player> players = new HashSet<>();
+        World world = sender.getWorld();
+        for(Player p : world.getPlayers()){
+            if(sender.getLocation().distance(p.getLocation()) < distance){
+                players.add(p);
+            }
+        }
+        return players;
     }
 
     @Override
     public void broadcastMessage(String message) {
-
+        for(Player p : members){
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
     }
 
     @Override
     public void closeChannel() {
-
+        channelManager.unregisterChannel(this);
     }
 
     @Override
     public String getFormat() {
-        return null;
+        return format;
     }
 
     @Override
     public String getDefaultColor() {
-        return null;
+        return defaultColor;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public void setDefaultColor(String defaultColor) {
-
+        this.defaultColor = defaultColor;
     }
 
     @Override
     public void setFormat(String format) {
-
+        this.format = format;
     }
 
     @Override
     public void addPlayer(Player player) {
-
+        members.add(player);
     }
 
     @Override
     public void removePlayer(Player player) {
-
+        members.remove(player);
     }
 
     @Override
     public Set<Player> getMembers() {
-        return null;
+        return new HashSet<>(members);
     }
 
     @Override
     public boolean doURLFilter() {
-        return false;
+        return doURLFilter;
     }
 
     @Override
     public boolean doProfanityFilter() {
-        return false;
+        return doProfanityFilter;
     }
 
     @Override
     public boolean doCooldown() {
-        //TODO: IMPLEMENT THIS
-        return false;
+        return doCooldown;
     }
 
     @Override
@@ -95,4 +167,3 @@ public class ProximityChannel implements Channel{
         return Type.PROXIMITY;
     }
 }
-
