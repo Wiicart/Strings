bcaea81b894895429f898c11d43771a1cb50e304
wiicart <wiicart@pedestriamc.com>
2024-08-06 19:27:28 -0500

    Update Channel.java

--- src/main/java/com/pedestriamc/strings/channels/Channel.java
+++ src/main/java/com/pedestriamc/strings/channels/Channel.java
@@ -1,11 +1,13 @@
 package com.pedestriamc.strings.channels;
 
+import com.pedestriamc.strings.User;
 import org.bukkit.entity.Player;
 
 import java.util.Map;
 import java.util.Set;
 
 public interface Channel {
+    
     void sendMessage(Player player, String message);
     void broadcastMessage(String message);
     void closeChannel();
@@ -17,10 +19,15 @@ public interface Channel {
     void setFormat(String format);
     void addPlayer(Player player);
     void removePlayer(Player player);
+    void addPlayer(User user);
+    void removePlayer(User user);
     Set<Player> getMembers();
     boolean doURLFilter();
     boolean doProfanityFilter();
     boolean doCooldown();
     Type getType();
+    void setEnabled(boolean isEnabled);
+    boolean isEnabled();
     Map<String, String> getData();
+    
 }
