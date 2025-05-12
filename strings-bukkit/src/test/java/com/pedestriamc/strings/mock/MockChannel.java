7bf1f014a101a381ae9fa5a7c303ffa4552b0dcb
christopher <sunglassesaj@gmail.com>
2025-05-12 17:39:12 -0400

    Try to fix workflows.

--- strings-bukkit/src/test/java/com/pedestriamc/strings/mock/MockChannel.java
+++ strings-bukkit/src/test/java/com/pedestriamc/strings/mock/MockChannel.java
@@ -3,11 +3,17 @@ package com.pedestriamc.strings.mock;
 import com.pedestriamc.strings.api.channel.Membership;
 import com.pedestriamc.strings.api.channel.Type;
 import com.pedestriamc.strings.channel.base.ProtectedChannel;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permissible;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public class MockChannel extends ProtectedChannel {
+
+    @Override
+    public void sendMessage(@NotNull Player player, @NotNull String message) {
+        System.out.println("<" + player.getName() + "> " + message);
+    }
 
     public MockChannel(String name) {
         super(name);
