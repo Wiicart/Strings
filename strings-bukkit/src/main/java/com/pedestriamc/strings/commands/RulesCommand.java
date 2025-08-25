6cc9cce3ee71cc22b369b9d9fe6940f4c89fa938
christopher <sunglassesaj@gmail.com>
2025-08-25 17:04:44 -0400

    Add a /rules command

---
+++ strings-bukkit/src/main/java/com/pedestriamc/strings/commands/RulesCommand.java
@@ -0,0 +1,32 @@
+package com.pedestriamc.strings.commands;
+
+import com.pedestriamc.strings.Strings;
+import com.pedestriamc.strings.api.settings.Option;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class RulesCommand implements CommandExecutor {
+
+    private final Strings strings;
+
+    private final Component message;
+
+    public RulesCommand(@NotNull Strings strings) {
+        this.strings = strings;
+
+        String raw = strings.getConfiguration().getString(Option.Text.RULES_MESSAGE);
+        message = MiniMessage.miniMessage().deserialize(raw);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        strings.adventure()
+                .sender(sender)
+                .sendMessage(message);
+        return true;
+    }
+}
