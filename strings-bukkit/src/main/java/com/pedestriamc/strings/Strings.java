c0c1480fd806144cb2de19f6df36cde39facfe02
christopher <sunglassesaj@gmail.com>
2026-05-03 18:00:57 -0400

    Set DISTRIBUTOR to github.

--- strings-bukkit/src/main/java/com/pedestriamc/strings/Strings.java
+++ strings-bukkit/src/main/java/com/pedestriamc/strings/Strings.java
@@ -58,7 +58,7 @@ public final class Strings extends JavaPlugin implements CommonStrings {
     public static final String VERSION = "1.7.1";
     public static final short VERSION_NUM = 8;
     public static final int METRICS_ID = 22597;
-    public static final String DISTRIBUTOR = "modrinth";
+    public static final String DISTRIBUTOR = "github";
 
     // Not reliant on config, so maintaining through reloads as not to break listeners
     private final StringsBukkitEventManager eventDispatcher = new StringsBukkitEventManager(this);
